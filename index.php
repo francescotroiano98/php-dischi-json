3a52ff66f351840249b97b68176f1a23d71ec7af
francescotroiano98 <francesco-troiano-98@libero.it>
2023-07-05 17:55:58 +0200

    added css style

--- index.php
+++ index.php
@@ -11,27 +11,27 @@
 
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
-        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="./styles/style.css">
     </head>
     <body>
 
         <div id="app">
-            <header>
+            <header class="d-flex align-items-center">
                 <div>
-                    <img src="" alt="">
+                    <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" alt="">
                 </div>
             </header>
             <main>
 
-                <div class="container">
+                <div class="container w-75">
 
                         <div class="row row-cols-1 row-cols-md-3 g-4">
                             <div class="col" v-for="(item, index) in discItems" :key="index" >
-                                <div class="card">
+                                <div class="card px-3 py-3">
                                 <img :src="discItems[index].poster" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ item.title }}</h5>
-                                    <p class="card-text">{{ item.author }}</p>
+                                <div class="card-body text-center px-0">
+                                    <h5 class="card-title mb-1">{{ item.title }}</h5>
+                                    <p class="card-text m-0 mb-1">{{ item.author }}</p>
                                     <h5 class="card-title">{{ item.year }}</h5>
                                 </div>
                                 </div>
